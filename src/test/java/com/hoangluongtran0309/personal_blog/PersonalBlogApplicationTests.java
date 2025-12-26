ff549ac063de46ce33bc148172be6a2294650022
hoangluongtran0309 <hoangluongtran0309@gmail.com>
2025-12-26 13:12:09 +0700

    chore: init personal blog project

---
+++ src/test/java/com/hoangluongtran0309/personal_blog/PersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoangluongtran0309.personal_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

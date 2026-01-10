e013d346641bb57b3b3b4538815456355b2c0707
hoangluongtran0309 <hoangluongtran0309@gmail.com>
2026-01-11 00:11:08 +0700

    test add child category should success

--- src/test/java/com/hoangluongtran0309/personal_blog/category/CategoryTest.java
+++ src/test/java/com/hoangluongtran0309/personal_blog/category/CategoryTest.java
@@ -3,6 +3,7 @@ package com.hoangluongtran0309.personal_blog.category;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
@@ -34,6 +35,19 @@ public class CategoryTest {
             Category.create(new CategoryId(UUID.randomUUID()), new CategoryName("New Category"),
                     new CategorySlug("new slug"));
         }, "new slug is invalid slug");
+    }
+
+    @Test
+    void addChildCategory_ShouldSuccess() {
+        Category parentCategory = Category.create(new CategoryId(UUID.randomUUID()),
+                new CategoryName("New Parent Category"),
+                new CategorySlug("new-parent-category"));
+        Category childCategory = Category.create(new CategoryId(UUID.randomUUID()),
+                new CategoryName("New Child Category"),
+                new CategorySlug("new-child-category"));
+        parentCategory.addChild(childCategory);
+        assertEquals("New Parent Category", childCategory.getParentCategory().getCategoryName().getName());
+        assertFalse(parentCategory.getChildrenCategories().isEmpty());
     }
 
 }

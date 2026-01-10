d86ec21a8f0f97240f5a0807a267ef7bbd5ed344
hoangluongtran0309 <hoangluongtran0309@gmail.com>
2026-01-11 00:00:14 +0700

    add constant

---
+++ src/main/java/com/hoangluongtran0309/personal_blog/common/Constant.java
@@ -0,0 +1,10 @@
+package com.hoangluongtran0309.personal_blog.common;
+
+public final class Constant {
+    private Constant() {
+
+    }
+
+    public static final String SLUG_PATTERN = "^[a-z0-9]+(?:(?:-|_)+[a-z0-9]+)*$";
+    
+}

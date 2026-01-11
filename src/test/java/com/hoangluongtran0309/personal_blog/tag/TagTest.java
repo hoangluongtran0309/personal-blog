24e9dd907dcdf6f148e7e0a5e90a3902e9435cea
hoangluongtran0309 <hoangluongtran0309@gmail.com>
2026-01-11 16:49:14 +0700

    test create tag should failure when value objects is invalid

--- src/test/java/com/hoangluongtran0309/personal_blog/tag/TagTest.java
+++ src/test/java/com/hoangluongtran0309/personal_blog/tag/TagTest.java
@@ -1,10 +1,10 @@
 package com.hoangluongtran0309.personal_blog.tag;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import org.junit.jupiter.api.Test;
 
 public class TagTest {
@@ -15,6 +15,23 @@ public class TagTest {
         assertNotNull(tag);
         assertEquals("New Tag", tag.getTagName().getName());
         assertEquals("new-tag", tag.getTagSlug().getSlug());
+    }
+
+    @Test
+    void createTag_ShouldFailure_WhenValueObjectsIsInvalid() {
+        assertThrowsExactly(IllegalArgumentException.class, () -> {
+            Tag.create(new TagId(null), new TagName("New Tag"), new TagSlug("new-Tag"));
+        }, "Tag ID must not be null");
+        assertThrowsExactly(IllegalArgumentException.class, () -> {
+            Tag.create(new TagId(UUID.randomUUID()), new TagName(""), new TagSlug("new-tag"));
+        }, "Tag name must not be blank");
+        assertThrowsExactly(IllegalArgumentException.class, () -> {
+            Tag.create(new TagId(UUID.randomUUID()), new TagName("New Tag"), new TagSlug(""));
+        }, "Tag slug must not be blank");
+        assertThrowsExactly(IllegalArgumentException.class, () -> {
+            Tag.create(new TagId(UUID.randomUUID()), new TagName("New Tag"),
+                    new TagSlug("new slug"));
+        }, "new slug is invalid slug");
     }
 
 }

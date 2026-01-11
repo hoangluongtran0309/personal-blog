799a3cb8e24d32bd265c0a26e6b2cb5e58294280
hoangluongtran0309 <hoangluongtran0309@gmail.com>
2026-01-11 17:57:30 +0700

    test create post with default draft status should failure when value objects is invalid

--- src/test/java/com/hoangluongtran0309/personal_blog/post/PostTest.java
+++ src/test/java/com/hoangluongtran0309/personal_blog/post/PostTest.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import org.junit.jupiter.api.Test;
 
 public class PostTest {
@@ -14,6 +15,17 @@ public class PostTest {
         assertNotNull(post);
         assertEquals(PostStatus.DRAFT, post.getPostStatus());
         assertEquals("New Post Title", post.getPostTitle().getTitle());
+    }
+
+    @Test
+    void createPost_WithDefaultDraftStatus_ShouldFailure_WhenValueObjectsIsInvalid() {
+        assertThrowsExactly(IllegalArgumentException.class, () -> {
+            Post.create(new PostId(null), new PostTitle("New Post Title"));
+        }, "Post ID must not be null");
+        assertThrowsExactly(IllegalArgumentException.class, () -> {
+            Post.create(new PostId(UUID.randomUUID()), new PostTitle(""));
+
+        }, "Post title must not be blank");
     }
 
 }

c64b23fcd9b6c9062d06d45e050727628270a8ef
hoangluongtran0309 <hoangluongtran0309@gmail.com>
2026-01-11 23:09:00 +0700

    test update project should success when project is draft status

--- src/test/java/com/hoangluongtran0309/personal_blog/project/ProjectTest.java
+++ src/test/java/com/hoangluongtran0309/personal_blog/project/ProjectTest.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import org.junit.jupiter.api.Test;
 
@@ -25,6 +26,18 @@ public class ProjectTest {
         assertThrowsExactly(IllegalArgumentException.class, () -> {
             Project.create(new ProjectId(UUID.randomUUID()), new ProjectTitle(""));
         }, "Project title must not be blank");
+    }
+
+    @Test
+    void updateProject_ShouldSuccess_WhenProjectIsDraftStatus() {
+        Project project = Project.create(new ProjectId(UUID.randomUUID()), new ProjectTitle("New Project Title"));
+        assertNull(project.getProjectBody());
+        assertNull(project.getProjectSlug());
+        project.update(new ProjectTitle("New Updated Project Title"), new ProjectBody("New Updated Project Summary", "New Updated Project Content"), new ProjectSlug("new-updated-project-slug"));
+        assertEquals("New Updated Project Title", project.getProjectTitle().getTitle());
+        assertEquals("New Updated Project Summary", project.getProjectBody().getSummary());
+        assertEquals("New Updated Project Content", project.getProjectBody().getContent());
+        assertEquals("new-updated-project-slug", project.getProjectSlug().getSlug());
     }
 
 }

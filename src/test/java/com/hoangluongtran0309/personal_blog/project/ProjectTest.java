d11def7d750ea4e89c68e901182e2c1ea6d0a495
hoangluongtran0309 <hoangluongtran0309@gmail.com>
2026-01-11 23:04:28 +0700

    test create project with default draft status should failure when value objects is invalid

--- src/test/java/com/hoangluongtran0309/personal_blog/project/ProjectTest.java
+++ src/test/java/com/hoangluongtran0309/personal_blog/project/ProjectTest.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import org.junit.jupiter.api.Test;
 
 public class ProjectTest {
@@ -15,5 +16,15 @@ public class ProjectTest {
         assertEquals(ProjectStatus.DRAFT, project.getProjectStatus());
         assertEquals("New Project Title", project.getProjectTitle().getTitle());
     }
-    
+
+    @Test
+    void createProject_WithDefaultDraftStatus_ShouldFailure_WhenValueObjectsIsInvalid() {
+        assertThrowsExactly(IllegalArgumentException.class, () -> {
+            Project.create(new ProjectId(null), new ProjectTitle("New Project Title"));
+        }, "Project ID must not be null");
+        assertThrowsExactly(IllegalArgumentException.class, () -> {
+            Project.create(new ProjectId(UUID.randomUUID()), new ProjectTitle(""));
+        }, "Project title must not be blank");
+    }
+
 }

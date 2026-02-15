e574df03ffbbabea9f2cdab1ff3e370eaf027933
hoangluongtran0309 <hoangluongtran0309@gmail.com>
2026-02-15 23:01:40 +0700

    feat(infrastructure/persistence): add base entity and jpa configuration

---
+++ src/main/java/com/hoangluongtran0309/personal_blog/infrastructure/persistence/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.hoangluongtran0309.personal_blog.infrastructure.persistence;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+}
+
